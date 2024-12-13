b5c74535b755733cec2d72ba165f3a82858bc50e
LeeMccaffery <tiraanos@gmail.com>
2024-12-13 14:17:41 +0000

    Add files via upload

---
+++ assignment 3.R
@@ -0,0 +1,23 @@
+df<- read.csv("https://public.tableau.com/app/sample-data/HollywoodsMostProfitableStories.csv")
+
+df
+
+install.packages("tidyverse") 
+
+library(tidyverse)
+
+str(df)
+
+colSums(is.na(df))
+
+df <- df %>% drop_na()
+
+colSums(is.na(df))
+
+summary(df)
+
+ggplot(df, aes(x=Lead.Studio, y=Rotten.Tomatoes..)) + geom_point()+ scale_y_continuous(labels = scales::comma)+coord_cartesian(ylim = c(0, 110))+theme(axis.text.x = element_text(angle = 90))   
+
+ggplot(df, aes(x=Year)) + geom_bar()
+
+write.csv(df, "clean_df.csv") 
